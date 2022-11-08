5289a7ada841781f5250d29cda9be43d2fbaa3c4
ann-mary27 <ann.mary1127@gmail.com>
2022-11-08 13:02:36 +0530

    about

--- about.js
+++ about.js
@@ -1,14 +1,23 @@
 const button_more=document.querySelector('.view-more');
 const button_less=document.querySelector('.view-less');
 var evt = new MouseEvent('click');
-button_more.addEventListener('click',()=>{
+button_more.addEventListener('click', ()=>{
 //     var dots = document.getElementById("dots");
 //   var moreText = document.getElementById("more");
 //   var btnText = document.getElementById("myBtn");
-    button_more.style.visibility=button_more.style.visibility==="hidden"?"visible":"hidden";
-    // button_less.style.visibility=button_less.style.visibility==="hidden"?"visible":"hidden";
+if (button_more.style.visibility==="hidden") {
+     button_more.style.visibility="visible";
     button_more.style.transitionDuration="1500ms";
-    button_less.style.transitionDuration="500ms";
+    button_less.style.transitionDuration="50ms";
+} else {
+    button_more.style.visibility="hidden";
+    button_more.style.transitionDuration="50ms";
+    button_less.style.transitionDuration="1000ms";
+}
+
+    // button_less.style.visibility=button_less.style.visibility==="hidden"?"visible":"hidden";
+    
+    
 
 //   else {
 //     dots.style.display = "none";
