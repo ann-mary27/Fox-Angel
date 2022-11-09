340f5c52f68f1893897c428b1e3bc2becf36f7f6
ann-mary27 <ann.mary1127@gmail.com>
2022-11-09 14:52:12 +0530

    doing last last now

--- index.js
+++ index.js
@@ -8,55 +8,118 @@ const p_url=[p1.getAttribute('src'),p2.getAttribute('src'),p3.getAttribute('src'
 
 const r_url=['./images/r1.jpg',"./images/r2.jpg","./images/r3.jpg","./images/r4.jpg"]
 
-p1.addEventListener('mouseover',(e)=>{
-
+p1.addEventListener('mouseover',()=>{
   p1.setAttribute('src',r_url[0]);
-
 });
 
-p2.addEventListener('mouseover',(e)=>{
-
+p2.addEventListener('mouseover',()=>{
   p2.setAttribute('src',r_url[1]);
-
 });
 
-p3.addEventListener('mouseover',(e)=>{
- 
+p3.addEventListener('mouseover',()=>{
   p3.setAttribute('src',r_url[2]);
-
 });
 
-p4.addEventListener('mouseover',(e)=>{
-
+p4.addEventListener('mouseover',()=>{
   p4.setAttribute('src',r_url[3]);
-
 });
 
 
-p1.addEventListener('mouseout',(e)=>{
-
+p1.addEventListener('mouseout',()=>{
   p1.setAttribute('src',p_url[0]);
-
 });
 
-p2.addEventListener('mouseout',(e)=>{
-
+p2.addEventListener('mouseout',()=>{
   p2.setAttribute('src',p_url[1]);
-
 });
 
-p3.addEventListener('mouseout',(e)=>{
- 
+p3.addEventListener('mouseout',()=>{
   p3.setAttribute('src',p_url[2]);
 
 });
 
-p4.addEventListener('mouseout',(e)=>{
-
+p4.addEventListener('mouseout',()=>{
   p4.setAttribute('src',p_url[3]);
-
 });
 
+
+
+// play video
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
+
+// gif-effect
+const g1=document.querySelector('#g1');
+const g2=document.querySelector('#g2');
+const g3=document.querySelector('#g3');
+const g4=document.querySelector('#g4');
+const g5=document.querySelector('#g5');
+const g6=document.querySelector('#g6');
+const g_url=["./images/g12.jpg","./images/g22.jpg","./images/g32.jpg","./images/g42.jpg","./images/g52.jpg","./images/g62.jpg"];
+const gr_url=[g1.getAttribute('src'),g2.getAttribute('src'),g3.getAttribute('src'),g4.getAttribute('src'),g5.getAttribute('src'),g6.getAttribute('src')];
+
+g1.addEventListener('mouseover',()=>{
+  g1.setAttribute('src',g_url[0]);
+});
+g1.addEventListener('mouseout',()=>{
+  g1.setAttribute('src',gr_url[0]);
+});
+
+g2.addEventListener('mouseover',()=>{
+  g2.setAttribute('src',g_url[1]);
+});
+g2.addEventListener('mouseout',()=>{
+  g2.setAttribute('src',gr_url[1]);
+});
+
+g3.addEventListener('mouseover',()=>{
+  g3.setAttribute('src',g_url[2]);
+});
+g3.addEventListener('mouseout',()=>{
+  g3.setAttribute('src',gr_url[2]);
+});
+
+g4.addEventListener('mouseover',()=>{
+  g4.setAttribute('src',g_url[3]);
+});
+g4.addEventListener('mouseout',()=>{
+  g4.setAttribute('src',gr_url[3]);
+});
+
+g5.addEventListener('mouseover',()=>{
+  g5.setAttribute('src',g_url[4]);
+});
+g5.addEventListener('mouseout',()=>{
+  g5.setAttribute('src',gr_url[4]);
+});
+
+g6.addEventListener('mouseover',()=>{
+  g6.setAttribute('src',g_url[5]);
+});
+g6.addEventListener('mouseout',()=>{
+  g6.setAttribute('src',gr_url[5]);
+});
 
 
 // slider 
