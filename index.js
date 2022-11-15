100e75e2f95f77d16e7fba42da49b322619b4fb2
ann-mary27 <ann.mary1127@gmail.com>
2022-11-16 04:27:00 +0530

    ..

--- index.js
+++ index.js
@@ -57,29 +57,43 @@ p4.addEventListener('mouseout',()=>{
 
 
 // play video
-window.document.onkeydown = function(e) {
-  if (!e) {
-    e = event;
-  }
-  if (e.keyCode == 27) {
-    lightbox_close();
-  }
-}
 
-function lightbox_open() {
-  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
-  window.scrollTo(0, 0);
-  document.getElementById('light').style.display = 'block';
-  document.getElementById('fade').style.display = 'block';
-  lightBoxVideo.play();
-}
 
-function lightbox_close() {
-  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
-  document.getElementById('light').style.display = 'none';
-  document.getElementById('fade').style.display = 'none';
-  lightBoxVideo.pause();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// window.document.onkeydown = function(e) {
+//   if (!e) {
+//     e = event;
+//   }
+//   if (e.keyCode == 27) {
+//     lightbox_close();
+//   }
+// }
+
+// function lightbox_open() {
+//   var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+//   window.scrollTo(0, 0);
+//   document.getElementById('light').style.display = 'block';
+//   document.getElementById('fade').style.display = 'block';
+//   lightBoxVideo.play();
+// }
+
+// function lightbox_close() {
+//   var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+//   document.getElementById('light').style.display = 'none';
+//   document.getElementById('fade').style.display = 'none';
+//   lightBoxVideo.pause();
+// }
 
 // gif-effect
 const g1=document.querySelector('#g1');
